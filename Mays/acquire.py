9b50e15646f33299e278917e72545b5a2158dea5
Daniella Bojado <daniella.bojado@gmail.com>
2020-11-20 02:00:47 -0600

    add file

---
+++ Mays/acquire.py
@@ -0,0 +1,129 @@
+# Acquire File
+# Imports
+
+import pandas as pd
+import numpy as np
+from requests import get
+from os import path
+from bs4 import BeautifulSoup
+import os
+import json
+from typing import Dict, List, Optional, Union, cast
+import requests
+from env import github_token, github_username
+
+################## Acquire Helper Functions ##################
+def make_soup(url):
+    '''
+    This helper function takes in a url and requests and parses HTML returning a soup object.
+    '''
+    headers = {'User-Agent': 'Codeup Data Science'} 
+    response = get(url, headers=headers)    
+    soup = BeautifulSoup(response.text, 'html.parser')
+    return soup
+
+def get_all_urls():
+    '''
+    This function scrapes all of the OpenCV repo urls from the Github and returns a list of urls.
+    '''
+    i = range(1,30)
+    repos = []
+    for num in i:
+        # The base url for the Github search page
+        url = f'https://github.com/search?o=desc&p={num}&q=OpenCV&s=stars&type=Repositories'
+        # Make request and soup object using helper
+        soup = make_soup(url)
+        # Create a list of the anchor elements that hold the urls.
+        urls_list = soup.find_all('a', class_='v-align-middle')
+        # I'm using a set comprehension to return only unique urls because list contains duplicate urls.
+        new_urls = {link.get('href') for link in urls_list}
+        # I'm converting and adding my set to a list of urls.
+        repos = repos + list(new_urls)
+    
+    # Removing first '/' character in all repo urls to fit into next function.
+    repos = [repo[1:] for repo in repos]
+        
+    return repos
+
+
+################## Acquire OpenCV Repo Data ##################
+
+# TODO: Make a github personal access token.
+#     1. Go here and generate a personal access token https://github.com/settings/tokens
+#        You do _not_ need select any scopes, i.e. leave all the checkboxes unchecked
+#     2. Save it in your env.py file under the variable `github_token`
+# TODO: Add your github username to your env.py file under the variable `github_username`
+# TODO: Add more repositories to the `REPOS` list below.
+
+headers = {"Authorization": f"token {github_token}", "User-Agent": github_username}
+
+if headers["Authorization"] == "token " or headers["User-Agent"] == "":
+    raise Exception( "You need to follow the instructions marked as TODO in this script before trying to use")
+
+def github_api_request(url: str) -> Union[List, Dict]:
+    response = requests.get(url, headers=headers)
+    response_data = response.json()
+    if response.status_code != 200:
+        raise Exception(
+            f"Error response from github api! status code: {response.status_code}, "
+            f"response: {json.dumps(response_data)}"
+        )
+    return response_data
+
+def get_repo_language(repo: str) -> str:
+    url = f"https://api.github.com/repos/{repo}"
+    repo_info = github_api_request(url)
+    if type(repo_info) is dict:
+        repo_info = cast(Dict, repo_info)
+        return repo_info.get("language", None)
+    raise Exception(
+        f"Expecting a dictionary response from {url}, instead got {json.dumps(repo_info)}"
+    )
+
+def get_repo_contents(repo: str) -> List[Dict[str, str]]:
+    url = f"https://api.github.com/repos/{repo}/contents/"
+    contents = github_api_request(url)
+    if type(contents) is list:
+        contents = cast(List, contents)
+        return contents
+    raise Exception(
+        f"Expecting a list response from {url}, instead got {json.dumps(contents)}"
+    )
+
+def get_readme_download_url(files: List[Dict[str, str]]) -> str:
+    """
+    Takes in a response from the github api that lists the files in a repo and
+    returns the url that can be used to download the repo's README file.
+    """
+    for file in files:
+        if file["name"].lower().startswith("readme"):
+            return file["download_url"]
+    return ""
+
+def process_repo(repo: str) -> Dict[str, str]:
+    """
+    Takes a repo name like "gocodeup/codeup-setup-script" and returns a
+    dictionary with the language of the repo and the readme contents.
+    """
+    contents = get_repo_contents(repo)
+    readme_download_url = get_readme_download_url(contents)
+    if readme_download_url == "":
+        readme_contents = None
+    else:
+        readme_contents = requests.get(readme_download_url).text
+    return {
+        "repo": repo,
+        "language": get_repo_language(repo),
+        "readme_contents": readme_contents,
+    }
+
+def scrape_github_data() -> List[Dict[str, str]]:
+    """
+    Loop through all of the repos and process them. Returns the processed data.
+    """
+    REPOS = get_all_urls()
+    return [process_repo(repo) for repo in REPOS]
+
+if __name__ == "__main__":
+    data = scrape_github_data()
+    json.dump(data, open("data.json", "w"), indent=1)
